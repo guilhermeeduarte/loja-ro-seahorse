ac8a1afaba9ab6ad46687dfecc2caf0606cfd073
Henrique Ribeiro <henriqueglribeiro@gmail.com>
2025-09-15 23:59:28 -0300

    Inicio? De algo?

---
+++ api/Amigurumi/src/main/java/com/roseahorse/Amigurumi/AmigurumiApplication.java
@@ -0,0 +1,13 @@
+package com.roseahorse.Amigurumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigurumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigurumiApplication.class, args);
+	}
+
+}
