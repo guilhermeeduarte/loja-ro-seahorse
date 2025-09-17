fc9a8393090a14b171ffcc8b0e6f8881f83e69f2
Henrique Ribeiro <henriqueglribeiro@gmail.com>
2025-09-17 01:25:53 -0300

    Criação da classe Produto e organização das pastas de controller, model e afins.

---
+++ api/Amigurumi/src/main/java/com/roseahorse/Amigurumi/model/Produto.java
@@ -0,0 +1,47 @@
+package com.roseahorse.Amigurumi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double valor;
+
+    public Produto(String nome, String descricao, Double valor) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+
+}
