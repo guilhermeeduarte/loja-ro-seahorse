32c4a8a82925c59c593622617cf943b3f26d7f12
Henrique Ribeiro <henriqueglribeiro@gmail.com>
2025-10-25 18:42:51 -0300

    bah

--- api/Amigurumi/src/main/java/com/roseahorse/Amigurumi/repository/ItemCarrinhoRepository.java
+++ api/Amigurumi/src/main/java/com/roseahorse/Amigurumi/repository/ItemCarrinhoRepository.java
@@ -1,11 +1,8 @@
 package com.roseahorse.Amigurumi.repository;
 
 import com.roseahorse.Amigurumi.model.ItemCarrinho;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,10 +14,7 @@ public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, Long
     Optional<ItemCarrinho> findByUsuario_IdAndProduto_Id(Long usuarioId, Long produtoId);
 
     @Modifying
-    @Transactional
-    @Query("DELETE FROM ItemCarrinho i WHERE i.usuario.id = :usuarioId")
-    void deleteByUsuario_Id(@Param("usuarioId") Long usuarioId);
-
+    void deleteByUsuarioId(Long usuarioId);
 
     long countByUsuarioId(Long usuarioId);
 
