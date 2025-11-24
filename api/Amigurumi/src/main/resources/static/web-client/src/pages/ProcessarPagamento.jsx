d1a9ec4aea6429425e6f594eaf377ef96e51e071
Guilherme Duarte <guilhermeduartegd2005@gmail.com>
2025-11-24 01:22:11 -0300

    Não foi...

--- api/Amigurumi/src/main/resources/static/web-client/src/pages/ProcessarPagamento.jsx
+++ api/Amigurumi/src/main/resources/static/web-client/src/pages/ProcessarPagamento.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { API_URL } from '../config/api';
+import { API_URL } from '../config/api'
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { useCarrinho } from '../contexts/CartContext'
-import { API_URL } from '../config/api'
 import '../styles.css'
 
 const ProcessarPagamento = () => {
